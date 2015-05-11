3b2763699392d0903ded409ce364cb6cbe6d8cd9
Maxime Horcholle <maxime.horcholle@gmail.com>
2015-05-12 00:10:43 +0200

    try logrus

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+    log "github.com/Sirupsen/logrus"
+)
+
+func main() {
+	f, _ := os.OpenFile("../log/app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	log.SetOutput(f)
+	log.SetFormatter(&log.TextFormatter{DisableColors: true})
+	log.Debug("debug")
+	log.Info("info")
+	log.Warn("warning")
+	log.Error("error")
+	//log.Fatal("fatal") On fatal app is shutdown
+	//log.Panic("panic") On panic app is shutdown
+	fmt.Println("Hello World")
+}
