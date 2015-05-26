60e31ac6edaabcf2bb26c500cadaf52a04d2edac
Christophe Lebettre <lebettre.christophe@gmail.com>
2015-05-27 00:30:11 +0200

    try to establish a connection betwenn the go application and mongodb (local)

---
+++ src/mongo.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"api/src/document"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"gopkg.in/mgo.v2"
+)
+
+// theses vars should be created dynamically
+var (
+	dbUrl  = "127.0.0.1:27017"
+	dbName = "miniwaveme"
+)
+
+func main() {
+	// create session
+	session, err := mgo.Dial(dbUrl)
+
+	// should return an error if err != nil
+	if err != nil {
+		log.Error("can't establish the connection to the database")
+		return
+	}
+	defer session.Close()
+
+	// Collection 'track'
+	tracks := session.DB(dbName).C("track")
+
+	// insert a new track
+	track := document.Track{Number: 1, Name: "Nice track !", Duration: 360}
+	err = tracks.Insert(track)
+
+	if err != nil {
+		log.Error("can't insert document")
+		return
+	}
+
+	// find all tracks
+	var findTracks []document.Track
+	err = tracks.Find(track).All(&findTracks)
+
+	// print result
+	fmt.Println(findTracks)
+}
