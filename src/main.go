00c66c4dc98beb3b216596912288041f2e86ef67
christophe <lebettre.christophe@gmail.com>
2015-07-01 02:09:35 +0200

    simplify routes declaration

--- src/main.go
+++ src/main.go
@@ -1,30 +1,23 @@
 package main
 
 import (
-	"fmt"
-	config "github.com/miniwaveme/api/src/config"
-	logger "github.com/miniwaveme/api/src/logger"
 	"github.com/julienschmidt/httprouter"
-	"github.com/minwaveme/api/src/config"
-	"github.com/miniwaveme/api/src/route/v1"
+	"github.com/miniwaveme/api/src/config"
+	"github.com/miniwaveme/api/src/logger"
+	"github.com/miniwaveme/api/src/route"
 	"net/http"
 	"os"
 )
 
 func main() {
 	config.LoadConfig()
-	conf := config.GetConfig()
-	fmt.Print(conf.GetBool("miniwaveme.log.enabled"))
 
 	logger.LoadLogger()
 	log := logger.GetLogger()
 	log.Info("application started")
 
-    r := httprouter.New()
-	route.RegisterAlbumRoutes(r)
-	route.RegisterArtistRoutes(r)
-	route.RegisterKeyRoutes(r)
-	route.RegisterTrackRoutes(r)
+	r := httprouter.New()
+	route.RegisterRoutesV1(r)
 
-	http.ListenAndServe("localhost"+":"+os.Getenv("API_PORT"), r)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("API_PORT"), r))
 }
---
+++ src/route/routes_v1.go
@@ -0,0 +1,41 @@
+package route
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"github.com/miniwaveme/api/src/controller"
+)
+
+func RegisterRoutesV1(r *httprouter.Router) {
+
+	// Register app keys routes
+	kc := controller.NewKeyController()
+	r.GET("/v1/key/list/:page", kc.GetKeyList)
+	r.GET("/v1/key/get/:id", kc.GetKey)
+	r.PUT("/v1/key/:id", kc.UpdateKey)
+	r.POST("/v1/key", kc.CreateKey)
+	r.DELETE("/v1/key/:id", kc.RemoveKey)
+
+	// Register tracks routes
+	tc := controller.NewTrackController()
+	r.GET("/v1/track/list/:page", tc.GetTrackList)
+	r.GET("/v1/track/get/:id", tc.GetTrack)
+	r.PUT("/v1/track/:id", tc.UpdateTrack)
+	r.POST("/v1/track", tc.CreateTrack)
+	r.DELETE("/v1/track/:id", tc.RemoveTrack)
+
+	// Register artists routes
+	arc := controller.NewArtistController()
+	r.GET("/v1/artist/list/:page", arc.GetArtistList)
+	r.GET("/v1/artist/get/:id", arc.GetArtist)
+	r.PUT("/v1/artist/:id", arc.UpdateArtist)
+	r.POST("/v1/artist", arc.CreateArtist)
+	r.DELETE("/v1/artist/:id", arc.RemoveArtist)
+
+	// Register albums routes
+	alc := controller.NewAlbumController()
+	r.GET("/v1/album/list/:page", alc.GetAlbumList)
+	r.GET("/v1/album/get/:id", alc.GetAlbum)
+	r.PUT("/v1/album/:id", alc.UpdateAlbum)
+	r.POST("/v1/album", alc.CreateAlbum)
+	r.DELETE("/v1/album/:id", alc.RemoveAlbum)
+}
