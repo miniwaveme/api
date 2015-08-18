230ef0f6f65711906d72a825bbc7217b66419241
Christophe Lebettre <lebettre.christophe@gmail.com>
2015-08-19 01:46:45 +0200

    enhancing mgo impl

--- src/main.go
+++ src/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/miniwaveme/api/src/config"
 	"github.com/miniwaveme/api/src/logger"
+	"github.com/miniwaveme/api/src/mongo"
 	"github.com/miniwaveme/api/src/route"
 	"net/http"
 )
@@ -17,6 +19,10 @@ func main() {
 
 	r := httprouter.New()
 	route.RegisterRoutesV1(r)
+
+	ds := mongo.Ds
+	mgoDatabase := ds.GetSession().DB(ds.GetDbName())
+	fmt.Println(mgoDatabase.Name)
 
 	log.Fatal(http.ListenAndServe(":"+conf.GetString("api_port"), r))
 }
--- src/mongo/mongo.go
+++ src/mongo/mongo.go
@@ -6,32 +6,53 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
+// ------------------------------------------------------------------------------------------------
+
+var Ds = initDataStore()
+
+func initDataStore() *DataStore {
+	return &DataStore{newSession(), getDbName()}
+}
+
 type DataStore struct {
 	session *mgo.Session
+	dbName  string
 }
 
-var (
-	dataStore = initDataStore()
-)
-
-func GetSession() *mgo.Session {
-	return dataStore.session.Copy()
+func (ds DataStore) GetSession() *mgo.Session {
+	return ds.session.Copy()
 }
+
+func (ds DataStore) GetDbName() string {
+	return ds.dbName
+}
+
+// ------------------------------------------------------------------------------------------------
 
 func newSession() *mgo.Session {
-
-	appConfig := config.GetConfig()
-
-	session, err := mgo.Dial(fmt.Sprintf("%s:%s/%s", appConfig.GetString("db_url"), appConfig.GetString("db_port"), config.GetConfig().GetString("db_name")))
+	session, err := mgo.Dial(getUrl())
 	if err != nil {
 		panic(err)
 	}
 
 	session.SetMode(mgo.Monotonic, true)
-
 	return session
 }
 
-func initDataStore() *DataStore {
-	return &DataStore{newSession()}
+// ------------------------------------------------------------------------------------------------
+
+func getUrl() string {
+	return fmt.Sprintf("%s:%s/%s", getDbUrl(), getDbPort(), getDbName())
+}
+
+func getDbUrl() string {
+	return config.GetConfig().GetString("db_url")
+}
+
+func getDbName() string {
+	return config.GetConfig().GetString("db_name")
+}
+
+func getDbPort() string {
+	return config.GetConfig().GetString("db_port")
 }
