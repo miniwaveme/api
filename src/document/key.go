452b3943bbcba8ca7f020a6f910ff38a0e6d59c0
Maxime Horcholle <maxime.horcholle@gmail.com>
2015-08-26 20:02:44 +0200

    change id from string  to bson.ObjectId

--- src/document/key.go
+++ src/document/key.go
@@ -1,8 +1,12 @@
 package document
 
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
 type Key struct {
-	Id        string   `json:"id" bson:"_id"`
-	AppKey    string   `json:"app_key" bson:"app_key"`
-	AppSecret string   `json:"app_secret" bson:"app_secret"`
-	Roles     []string `json:"roles" bson:"roles"`
+	Id        bson.ObjectId `json:"id" bson:"_id"`
+	AppKey    string        `json:"app_key" bson:"app_key"`
+	AppSecret string        `json:"app_secret" bson:"app_secret"`
+	Roles     []string      `json:"roles" bson:"roles"`
 }
--- src/document/track.go
+++ src/document/track.go
@@ -1,13 +1,17 @@
 package document
 
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
 type Track struct {
-	Id       string   `json:"id" bson:"_id"`
-	Number   int      `json:"number" bson:"number"`
-	Name     string   `json:"name" bson:"name"`
-	Artists  []Artist `json:"artists" bson:"artists"`
-	Duration int      `json:"duration" bson:"duration"`
-	Bpm      int      `json:"bpm" bson:"bpm"`
-	Album    Album    `json:"album" bson:"album"`
-	Path     string   `json:"path" bson:"path"`
-	Url      string   `json:"url" bson:"url"`
+	Id       bson.ObjectId `json:"id" bson:"_id"`
+	Number   int           `json:"number" bson:"number"`
+	Name     string        `json:"name" bson:"name"`
+	Artists  []Artist      `json:"artists" bson:"artists"`
+	Duration int           `json:"duration" bson:"duration"`
+	Bpm      int           `json:"bpm" bson:"bpm"`
+	Album    Album         `json:"album" bson:"album"`
+	Path     string        `json:"path" bson:"path"`
+	Url      string        `json:"url" bson:"url"`
 }
